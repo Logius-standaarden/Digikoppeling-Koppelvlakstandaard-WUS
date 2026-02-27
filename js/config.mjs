4e96dd158b166d74053e9a88432233e6043ec215
Alexander Green <alexander.green@logius.nl>
2026-02-27 12:49:59 +0100

    PDF-naam niet meer in config

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "Digikoppeling-Koppelvlakstandaard-WUS.pdf"
-      } ],
   authors: [ { 
         "mailto" : "digikoppeling@logius.nl",
         "name" : "Logius"
